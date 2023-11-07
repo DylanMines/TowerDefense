00fb71bfd9373c8f08127f8e203ee94437d52a5e
DylanMines <dylan.e.mines@gmail.com>
2023-11-07 09:19:52 -0700

    upload files

---
+++ Towers/attacks.lua
@@ -0,0 +1,8 @@
+local M = {}
+
+M.Bomb = {
+    ["ProjSprite"] = }
+
+
+
+return M
---
+++ enemies/enemy.go
@@ -0,0 +1,51 @@
+components {
+  id: "enemy_control"
+  component: "/enemies/enemy_control.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "death"
+  component: "/enemies/death.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/enemies/characters.atlas\"\n"
+  "default_animation: \"Enemy\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "playback_rate: 3.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
