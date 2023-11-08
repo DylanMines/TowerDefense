59fbbc060ebbdc8e8ad3f0f1bf1306274c39442d
DylanMines <dylan.e.mines@gmail.com>
2023-11-08 09:16:23 -0700

    enemy health effects

--- enemies/enemy.go
+++ enemies/enemy.go
@@ -14,21 +14,6 @@ components {
   }
 }
 components {
-  id: "enemy_health"
-  component: "/Gui/enemy_health.gui"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-components {
   id: "enemy_control"
   component: "/enemies/enemy_control.script"
   position {
