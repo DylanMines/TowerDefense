76bb3080c3307c2247e01346aa229a6f4b6bcc43
DylanMines <dylan.e.mines@gmail.com>
2024-04-07 10:19:17 -0600

    testing out sublime merge

--- Towers/Bomb/bomb.go
+++ Towers/Bomb/bomb.go
@@ -1,6 +1,11 @@
-components {
-  id: "Tower_class"
-  component: "/Towers/Tower_class.script"
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/Towers/Towers.atlas\"\n"
+  "default_animation: \"Bomb\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
   position {
     x: 0.0
     y: 0.0
@@ -12,19 +17,36 @@ components {
     z: 0.0
     w: 1.0
   }
-  properties {
-    id: "range"
-    value: "120.0"
-    type: PROPERTY_TYPE_NUMBER
+}
+embedded_components {
+  id: "projectile_spawner"
+  type: "factory"
+  data: "prototype: \"/Towers/Bomb/bomb_projectile.go\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
   }
 }
 embedded_components {
-  id: "sprite"
-  type: "sprite"
-  data: "tile_set: \"/Towers/Towers.atlas\"\n"
-  "default_animation: \"Bomb\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
+  id: "shoot"
+  type: "sound"
+  data: "sound: \"/Towers/Bomb/shoot.wav\"\n"
+  "looping: 0\n"
+  "group: \"sound_fx\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
   ""
   position {
     x: 0.0
