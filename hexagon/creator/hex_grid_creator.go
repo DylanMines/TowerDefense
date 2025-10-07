47af98167f821e665e992d20c23f9a80a4c1d7e2
DylanMines <dylan.e.mines@gmail.com>
2025-10-07 11:04:16 -0600

    Added hex_tile_factory

--- hexagon/creator/hex_grid_creator.go
+++ hexagon/creator/hex_grid_creator.go
@@ -6,3 +6,7 @@ components {
   id: "hex_grid_creator"
   component: "/hexagon/creator/hex_grid_creator.script"
 }
+components {
+  id: "hex_tile_factory"
+  component: "/hexagon/tile/hex_tile_factory.factory"
+}
