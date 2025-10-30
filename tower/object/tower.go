abaa62ed6252c6df4b405e59221ee8bf8b48a2e9
DylanMines <dylan.e.mines@gmail.com>
2025-10-30 11:47:42 -0600

    moved things from Building to tower

--- tower/object/tower.go
+++ tower/object/tower.go
@@ -1,24 +1,15 @@
 embedded_components {
   id: "Tower"
   type: "sprite"
-  data: "default_animation: \"Level1\"\n"
+  data: "default_animation: \"Level 1\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
   "textures {\n"
   "  sampler: \"texture_sampler\"\n"
-  "  texture: \"/Building/StationSprites.tilesource\"\n"
+  "  texture: \"/tower/resources/tower_images.atlas\"\n"
   "}\n"
   ""
   position {
-    x: 0.0
-    y: 0.0
     z: 1.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
   }
 }
 embedded_components {
@@ -30,24 +21,14 @@ embedded_components {
   "size {\n"
   "  x: 150.0\n"
   "  y: 150.0\n"
-  "  z: 0.0\n"
-  "  w: 0.0\n"
   "}\n"
   "size_mode: SIZE_MODE_MANUAL\n"
   "textures {\n"
   "  sampler: \"texture_sampler\"\n"
-  "  texture: \"/Building/Station.atlas\"\n"
+  "  texture: \"/tower/resources/tower_images.atlas\"\n"
   "}\n"
   ""
   position {
-    x: 0.0
-    y: 0.0
     z: 1.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
   }
 }
