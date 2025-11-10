65cb9e1b40b4046577fae0a81d25776ca5d4f2e8
DylanMines <dylan.e.mines@gmail.com>
2025-11-10 11:43:06 -0700

    update tower to be it's own collection

--- tower/tower.go
+++ tower/tower.go
@@ -1,11 +1,3 @@
-components {
-  id: "tower"
-  component: "/tower/tower.script"
-}
-components {
-  id: "tower_window"
-  component: "/tower/tower.gui"
-}
 embedded_components {
   id: "sprite"
   type: "sprite"
@@ -16,27 +8,4 @@ embedded_components {
   "  texture: \"/tower/resources/tower_images.atlas\"\n"
   "}\n"
   ""
-  position {
-    z: 1.0
-  }
-}
-embedded_components {
-  id: "range"
-  type: "sprite"
-  data: "default_animation: \"range\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ADD\n"
-  "size {\n"
-  "  x: 150.0\n"
-  "  y: 150.0\n"
-  "}\n"
-  "size_mode: SIZE_MODE_MANUAL\n"
-  "textures {\n"
-  "  sampler: \"texture_sampler\"\n"
-  "  texture: \"/tower/resources/tower_images.atlas\"\n"
-  "}\n"
-  ""
-  position {
-    z: 1.0
-  }
 }
