89c5cdbc79b25f6ed4c51277c2645fa75233b55d
DylanMines <dylan.e.mines@gmail.com>
2025-11-18 11:55:33 -0700

    radio widget interface

---
+++ gui/widgets/radio/radio_widget.lua
@@ -0,0 +1,7 @@
+---@meta radio_widget
+--lua interface LOLOLOLOLOLOLOLOL
+---@class widget.radio_widget : druid.widget
+---@field on_state_changed event The event to subscribe to for state changes
+---@field set_state function Set the state of the radio widget
+---@field get_state function Get the state of the radio widget
+local radio_widget = {}
